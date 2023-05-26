b9cd1650494567db9e305bffc72c60dae7f66b42
Atoche Pascual <jmatochepascual@gmail.com>
2023-05-26 09:08:28 -1000

    [ADD] variables

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// * VARIABLES
+const inputMascota = document.querySelector('#mascota');
+const inputProietrio = document.querySelector('#propietario');
+const inputTelefono = document.querySelector('#telefono');
+const inputFecha = document.querySelector('#fecha');
+const inputHora = document.querySelector('#hora');
+const inputSintomas = document.querySelector('#sintomas');
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
+
+
+
+// * CLASES
+
+
+
+// * EVENTOS
+
+
+
+// * FUNCIONES
+
